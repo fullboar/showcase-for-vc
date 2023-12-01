fe1eac347cb26fd12755555ae3fae9617fb12762
Adam Harvey <adamharvey@fisgardlabs.com>
2023-11-30 16:05:17 -0800

    chore: typo fix (#15)
    Signed-off-by: Adam Harvey <adamharvey@fisgardlabs.com>

--- api/src/content/misc/Wallets.ts
+++ api/src/content/misc/Wallets.ts
@@ -3,7 +3,7 @@ import type { Wallet } from '../types'
 export const Wallets: Wallet[] = [
   {
     id: 1,
-    name: 'myGobNL Wallet',
+    name: 'myGovNL Wallet',
     organization: 'The Province of Newfoundland and Labrador',
     recommended: true,
     icon: '/public/wallets/icon-wallet.png',
